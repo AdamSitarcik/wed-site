9b985f73c8e204988bd3dff7c16e4dc23d736045
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-04-03 23:12:18 +0200

    remove element from top of site when in small mode

--- client/src/assets/wrappers/SmallNavbar.js
+++ client/src/assets/wrappers/SmallNavbar.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.aside`
-    height: var(--navbar-height);
+    height: 0;
     display: flex;
     flex-direction: column;
     position: relative;
