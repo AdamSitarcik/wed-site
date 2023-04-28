b4b6a9912c499ed6d61a9e6a44f9c81ac3b6aee8
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-04-28 15:42:10 +0200

    prepare for production

--- Routes.js
+++ Routes.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import rateLimiter from 'express-rate-limit';
+
 const router = express.Router();
+
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: 'Skúste opäť za 15 minút',
+});
 
 import { createGuest } from './Controller.js';
 
-router.route('/create-guest').post(createGuest);
+router.route('/create-guest').post(apiLimiter, createGuest);
 
 export default router;
--- server.js
+++ server.js
@@ -3,14 +3,38 @@ const app = express();
 app.use(express.json());
 import dotenv from 'dotenv';
 dotenv.config();
-import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 import connectDB from './ConnectDb.js';
 import guestRouter from './Routes.js';
 
-app.use('/api', guestRouter);
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-app.use(bodyParser.json());
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+app.use(express.json());
+
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.use('/api', guestRouter);
 
 const port = process.env.PORT || 5000;
 
